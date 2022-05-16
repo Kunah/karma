8de71d004c90bd1a844162bfe32a09fdd6d0de43
Kunah <reply.kunah@gmail.com>
2022-05-16 13:42:44 +0200

    ✔ Added test initialization

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -9,13 +9,21 @@ use PHPUnit\Framework\TestCase;
 include 'src/config/config.php';
 
 final class ExampleTest extends TestCase {
+
+    private $manager;
+
+    /**
+     * @before
+     */
+    public function initTestEnvironment(){
+        $this->manager = new \App\Models\ExampleManager();
+    }
+
     public function testExampleSuccess(){
-        $manager = new \App\Models\ExampleManager();
-        $this->assertEquals($manager->getData(), 'success_test');
+        $this->assertEquals($this->manager->getData(), 'success_test');
     }
 
     public function testExampleError(){
-        $manager = new \App\Models\ExampleManager();
-        $this->assertEquals($manager->getData(), 'error_test');
+        $this->assertEquals($this->manager->getData(), 'error_test');
     }
 }
