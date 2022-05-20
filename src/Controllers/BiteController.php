b87c1883632be8a5f952bed9a86a5f1dcf3ec2fc
Kunah <reply.kunah@gmail.com>
2022-05-20 09:04:05 +0200

    ✔ Removed useless files

--- src/Controllers/BiteController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Controllers;
-
-class BiteController extends Controller
-{
-  public function index(){
-    $this->render("your_view_name");
-  }
-}
--- src/Controllers/TestController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Controllers;
-
-class TestController extends Controller
-{
-  public function index(){
-    $this->render("your_view_name");
-  }
-}
