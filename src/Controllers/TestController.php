911aab8215d7f2535bc9cf2b161c2495dc0e4d0e
Kunah <reply.kunah@gmail.com>
2022-05-20 02:27:34 +0200

    ✔ Removed useless code

---
+++ src/Controllers/TestController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Controllers;
+
+class TestController extends Controller
+{
+  public function index(){
+    $this->render("your_view_name");
+  }
+}
---
+++ src/Models/Bite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+class Bite extends Model {
+
+  protected static $table = "example_table_name"; // Optional param
+  // Just set the name of each SQL table column and accessors will automatically be generated
+  protected $example;
+
+}
