daff2145e05f34f196195775b534e461a15108f3
Kunah <reply.kunah@gmail.com>
2022-05-24 16:40:49 +0200

    ✔ Removed useless route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 use App\Controllers\ExampleController;
-use App\Controllers\MewenController;
 use App\Http\Router;
 
 Router::get('/', [ExampleController::class, "index"]);
-Router::get('/mewen', [MewenController::class, "index"]);
 Router::get('/:param', [ExampleController::class, "index"]);
