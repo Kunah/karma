a8a7be8ed6372453c5b3ca16825fd8fe09493cc8
Kunah <reply.kunah@gmail.com>
2022-05-24 16:04:48 +0200

    ✔ Improved "setup" command

---
+++ src/Views/layout.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="title" value="App"/> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>App</title>
+    
+<script src='https://cdn.tailwindcss.com'></script>
+<script src='https://code.jquery.com/jquery-3.6.0.min.js' integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=' crossorigin='anonymous'></script>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <main>
+        <?php echo $content ?>
+    </main>
+</body>
+</html>
